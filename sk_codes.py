2c60f75a947942a766d210f23439db544312eba6
samanvitakanuparthi8-arch <samanvitakanuparthi8@gmail.com>
2026-04-27 09:02:25 +0530

    Create sk_codes.py with palindrome function

---
+++ sk_codes.py
@@ -0,0 +1,13 @@
+def is_palindrome(n):
+    original = n
+    reverse = 0
+
+    while n > 0:
+        digit = n % 10
+        reverse = reverse * 10 + digit
+        n //= 10
+
+    return original == reverse
+
+num = int(input("Enter a number: "))
+print("Palindrome" if is_palindrome(num) else "Not Palindrome")
